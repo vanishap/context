a77d15435461117165aec3bc8ec2d9d5d2dd6b78
vanishap <vanishareddyp@gmail.com>
2023-06-01 11:12:31 -0700

    props drilling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+
+// prop ddrilling-- passing state into different components to reach the 
+//child component that needs the state.
+
 function App() {
+  const [user, setUser] = useState('Vanisha');
+
+  const clickHandler= ()=>{ 
+      setUser('Vanisha Paidimarla') 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>Welcome <span style={{color:'blue'}}>{user}</span>, Happy coding</h1>
+   <button onClick={clickHandler}>Change name</button>
+   <Component1 user ={user}/>
     </div>
   );
 }
 
+const Component1 =({user})=>{
+  return (
+    <div>
+      <h2>Hello Component1</h2>
+      <Component2 user ={user}/>
+    </div>
+
+  )
+}
+const Component2= ({user})=>{
+  return (
+    <div>
+      <h2>Hello Component2</h2>
+      <Component3 user ={user}/>
+    </div>
+  )
+}
+const Component3= ({user})=>{
+  return (
+    <div>
+      <h2>Hello Component3</h2>
+      <Component4 user ={user}/>
+    </div>
+  )
+}
+const Component4= ({user})=>{
+  return (
+    <div>
+      <h2>Hello Component4</h2>
+      <h3>Hope you are enjoying the course {user}</h3>
+    </div>
+  )
+}
 export default App;
